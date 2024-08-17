725e6af20c76fbe88c4768ce962659b3cd0505f6
Tristan-SolarFlare <tristan@solarflarerobotics.org>
2024-08-16 22:45:12 -0400

    starship v3

--- project (2).js
+++ project (2).js
@@ -1,4 +1,7 @@
 const boosterHeight = 250; //default is set to 250. this can be changed
+const flameAngle = 90; //in degrees
+const flameLength = 50; //vertical length
+const flameLines = 5;
 
 const width = 150;
 const height = boosterHeight + 350;
@@ -35,6 +38,26 @@ t.goTo([85,height-90])
 t.jump([75, height-65])
 t.goTo([65,height-90])
 t.goTo([85,height-90])
+
+//draw body lines for detail
+t.jump([85, height-95])
+t.goTo([65,height-95])
+t.jump([85, height-100])
+t.goTo([65,height-100])
+t.jump([85, height-105])
+t.goTo([65,height-105])
+
+// draw front fins
+
+t.jump([70,height-250])
+t.goTo([70,height-235])
+t.goTo([67,height-235])
+t.goTo([67,height-250])
+
+t.jump([80,height-250])
+t.goTo([80,height-235])
+t.goTo([83,height-235])
+t.goTo([83,height-250])
 
 //draw right wing
 t.jump([95,height-250])
@@ -73,6 +96,26 @@ t.jump([55,height-250-boosterHeight])
 t.goTo([40,height-256-boosterHeight])
 t.goTo([40,height-230-boosterHeight])
 t.goTo([55,height-220-boosterHeight])
+
+// draw front fins
+
+t.jump([70,height-250-boosterHeight])
+t.goTo([70,height-230-boosterHeight])
+t.goTo([67,height-230-boosterHeight])
+t.goTo([67,height-250-boosterHeight])
+
+t.jump([80,height-250-boosterHeight])
+t.goTo([80,height-230-boosterHeight])
+t.goTo([83,height-230-boosterHeight])
+t.goTo([83,height-250-boosterHeight])
+
+//draw engines
+t.jump([65,height-250-boosterHeight])
+t.goTo([60,height-260-boosterHeight])
+t.goTo([90,height-260-boosterHeight])
+t.goTo([85,height-250-boosterHeight])
+
+
 
 
 bt.join(finalLines, t.lines());
